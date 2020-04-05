90e43f213916e51bf4360ff9f95f46ec1da6aeca
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-04-05 16:20:21 -0700

    Turn Touches into Lines

--- TouchTracker/View/DrawView.swift
+++ TouchTracker/View/DrawView.swift
@@ -35,5 +35,42 @@ class DrawView: UIView{
         }
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    // Line stores these points as properties named begin and end; when a touch begins, a Line will be created and set both of its properties to the point where the touch began.  When the touch moves, it will update with there the line ends.
+        
+        let touch = touches.first!
+        
+        // Getting the location of the touch n view's coordinate system
+        let location = touch.location(in: self)
+        
+        currentLine = Line(begin: location, end: location)
+        
+        setNeedsDisplay()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        
+        currentLine?.end = location
+        setNeedsDisplay()
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if var line = currentLine {
+            let touch = touches.first!
+            let location = touch.location(in: self)
+            line.end = location
+            
+            finishedLines.append(line)
+            // Appending the line that was just created to the finishedLines when the touch ends.
+        }
+        
+        currentLine = nil
+        setNeedsDisplay()
+    }
+    
+    
     
 }
